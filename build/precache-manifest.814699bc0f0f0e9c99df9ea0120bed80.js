d075aea52dfe3e98e4ac05db5c24add03a3680f7
KevinJoseph <kevramos95@gmail.com>
2020-12-07 17:32:30 -0500

    deploy

---
+++ build/precache-manifest.814699bc0f0f0e9c99df9ea0120bed80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91a1b001f7a1e5f8c3b12bef4b7dd031",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0a12b6e88509d6e64fc",
+    "url": "/static/css/2.7dc5a135.chunk.css"
+  },
+  {
+    "revision": "a0cd4928d4de596eba9a",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "c0a12b6e88509d6e64fc",
+    "url": "/static/js/2.c284350f.chunk.js"
+  },
+  {
+    "revision": "544111c69119227bbf075a65771ccdb1",
+    "url": "/static/js/2.c284350f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0cd4928d4de596eba9a",
+    "url": "/static/js/main.0d25648e.chunk.js"
+  },
+  {
+    "revision": "5bb41588006e9c718e8f",
+    "url": "/static/js/runtime-main.6c255e20.js"
+  }
+]);
